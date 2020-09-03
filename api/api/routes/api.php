dfcc77fe68149d59d3cacad661601fff85654ce7
rodrigoftw <rodrigoftw2@gmail.com>
2020-09-03 08:46:54 -0300

    Updated API routes

--- api/api/routes/api.php
+++ api/api/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +11,20 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+        'middleware' => 'auth:api',
+    ], function () {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
 });
+
+Route::apiResource('/products', 'ProductController')->only([
+    'index', 'store', 'show', 'update', 'destroy',
+]);
