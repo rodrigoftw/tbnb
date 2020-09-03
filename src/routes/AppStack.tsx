f0d31172a7c15ee4daa93d1efe7e0a2b2cfc1290
rodrigoftw <rodrigoftw2@gmail.com>
2020-09-03 01:58:59 -0300

    Added app routes

---
+++ src/routes/AppStack.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import ProductList from '../pages/ProductList';
+import ProductDetail from '../pages/ProductDetail';
+import AddProduct from '../pages/AddProduct';
+import EditProduct from '../pages/EditProduct';
+
+const { Navigator, Screen } = createStackNavigator();
+
+function AppStack() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="ProductList" component={ProductList} />
+        <Screen name="ProductDetail" component={ProductDetail} />
+        <Screen name="AddProduct" component={AddProduct} />
+        <Screen name="EditProduct" component={EditProduct} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppStack;
